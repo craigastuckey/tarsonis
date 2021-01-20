33fb57017e283fa66d5095f776e33ba054ea7ba4
craigastuckey <craigastuckey@gmail.com>
2021-01-20 14:33:07 -0600

    hello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hello World!
+          Hello Buttface!
         </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
